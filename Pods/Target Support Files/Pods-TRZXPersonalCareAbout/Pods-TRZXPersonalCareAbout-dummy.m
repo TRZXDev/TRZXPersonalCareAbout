7a4d7047e47cbd2c9601dbf790e4cc4741b4f43e
zhangjw@mmwipo.com <zhangjw@mmwipo.com>
2017-02-23 16:20:38 +0800

    first commit

---
+++ Pods/Target Support Files/Pods-TRZXPersonalCareAbout/Pods-TRZXPersonalCareAbout-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TRZXPersonalCareAbout : NSObject
+@end
+@implementation PodsDummy_Pods_TRZXPersonalCareAbout
+@end
---
+++ TRZXPersonalCareAbout/TRZXPersonalCareAbout/TRZXPersonalCareAboutController.h
@@ -0,0 +1,15 @@
+//
+//  TRZXPersonalCareAboutController.h
+//  TRZXPersonalCareAbout
+//
+//  Created by 张江威 on 2017/2/23.
+//  Copyright © 2017年 张江威. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TRZXPersonalCareAboutController : UIViewController
+
+@property (strong, nonatomic) NSString * midStr;
+
+@end
